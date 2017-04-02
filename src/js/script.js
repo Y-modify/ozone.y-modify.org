b192ea3a294af494a6d3dbecd64858a16ba1aabc
coord.e <me@coord-e.com>
2017-04-02 19:42:12 +0900

    Styled script and fixed icon's behavior

--- src/js/script.js
+++ src/js/script.js
@@ -9,80 +9,114 @@ require('./particle-setting.js');
 require('./lineart.js');
 
 let distlist = [{
-  "name": "debian",
-  "displayName": "Debian",
-},{
-  "name": "centos",
-  "displayName": "CentOS",
-},{
-  "name": "fedora",
-  "displayName": "Fedora",
+    "name": "debian",
+    "displayName": "Debian",
+}, {
+    "name": "centos",
+    "displayName": "CentOS",
+}, {
+    "name": "fedora",
+    "displayName": "Fedora",
 }];
 
-$(document).ready(function(){
-  var mySwiper = new Swiper ('.swiper-container', {
-   // Optional parameters
-   loop: true,
-   // If we need pagination
-   pagination: '.swiper-pagination',
-   // Navigation arrows
-   nextButton: '.swiper-button-next',
-   prevButton: '.swiper-button-prev',
-   autoplay: 2000,
-   autoplayDisableOnInteraction: false,
-   paginationClickable: true,
- });
+$(document).ready(function() {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        // If we need pagination
+        pagination: '.swiper-pagination',
+        // Navigation arrows
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        autoplay: 2000,
+        autoplayDisableOnInteraction: false,
+        paginationClickable: true,
+    });
 
- if(mySwiper.length > 0){
-  mySwiper.on("onSlideChangeStart", function(){
-    $('.top').removeClass('top');
-    $('#'+distlist[mySwiper.realIndex].name).addClass('top');
-  });
- }
+    if (mySwiper.on) {
+        mySwiper.on("onSlideChangeStart", function() {
+            $('.top').removeClass('top');
+            $('#' + distlist[mySwiper.realIndex].name).addClass('top');
+        });
+    }
 
- $('#btn-dl').on('mouseover', function(){
-   mySwiper.stopAutoplay();
- });
+    $('#btn-dl').on('mouseover', function() {
+        mySwiper.stopAutoplay();
+    });
 
- $('#btn-dl').on('mouseout',function(){
-   mySwiper.startAutoplay();
- });
+    $('#btn-dl').on('mouseout', function() {
+        mySwiper.startAutoplay();
+    });
 
- $('#btn-dl').on('click', function(){
-   location.href = "/download/#"+distlist[mySwiper.realIndex].name;
- });
+    $('#btn-dl').on('click', function() {
+        location.href = "/download/#" + distlist[mySwiper.realIndex].name;
+    });
 
- $('a[data-moveto]').on('click', function(){
-   $("html,body").animate({scrollTop:$($(this).attr("data-moveto")).offset().top});
- });
+    $('a[data-moveto]').on('click', function() {
+        $("html,body").animate({
+            scrollTop: $($(this).attr("data-moveto")).offset().top
+        });
+    });
 
- $('a[data-moveto]').on('touchend', function(){
-   $("html,body").animate({scrollTop:$($(this).attr("data-moveto")).offset().top});
- });
+    $('a[data-moveto]').on('touchend', function() {
+        $("html,body").animate({
+            scrollTop: $($(this).attr("data-moveto")).offset().top
+        });
+    });
 
- $('.closebtn').on('click', function(){
-   $("html,body").animate({scrollTop:0});
- });
+    $('.closebtn').on('click', function() {
+        $("html,body").animate({
+            scrollTop: 0
+        });
+    });
 
- $('.minbtn').on('click', function(){
-   $("html,body").animate({scrollTop:$(document).height()});
- });
+    $('.minbtn').on('click', function() {
+        $("html,body").animate({
+            scrollTop: $(document).height()
+        });
+    });
 
- let prevW = 0;
+    let prevW = 0;
 
- $('.maxbtn').on('click', function(){
-   if($("#termart").hasClass("maximized")){
-     $("#termart").removeClass("maximized");
-     $("#termart").animate({"width": prevW, "borderRadius":"5px"},{"queue":false});
-     $("#termart .topbar").animate({"borderTopRightRadius":"5px"},{"queue":false});
-     $("#termart .topbar").animate({"borderTopLeftRadius":"5px"},{"queue":false});
-     $("#termart .prompt").css({"display":"none"});
-   }else{
-     $("#termart").addClass("maximized");
-     prevW = $("#termart").css("width");
-     $("#termart").animate({"width": "100%", "borderRadius":"0"},{"queue":false});
-     $("#termart .topbar").animate({"borderRadius":"0"},{"queue":false});
-     $("#termart .prompt").css({"display":"inline"});
-   }
- });
+    $('.maxbtn').on('click', function() {
+        if ($("#termart").hasClass("maximized")) {
+            $("#termart").removeClass("maximized");
+            $("#termart").animate({
+                "width": prevW,
+                "borderRadius": "5px"
+            }, {
+                "queue": false
+            });
+            $("#termart .topbar").animate({
+                "borderTopRightRadius": "5px"
+            }, {
+                "queue": false
+            });
+            $("#termart .topbar").animate({
+                "borderTopLeftRadius": "5px"
+            }, {
+                "queue": false
+            });
+            $("#termart .prompt").css({
+                "display": "none"
+            });
+        } else {
+            $("#termart").addClass("maximized");
+            prevW = $("#termart").css("width");
+            $("#termart").animate({
+                "width": "100%",
+                "borderRadius": "0"
+            }, {
+                "queue": false
+            });
+            $("#termart .topbar").animate({
+                "borderRadius": "0"
+            }, {
+                "queue": false
+            });
+            $("#termart .prompt").css({
+                "display": "inline"
+            });
+        }
+    });
 });
